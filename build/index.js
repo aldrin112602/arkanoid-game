9e1254df66bb7dafb744e646d4a20868a1191ffc
Aldrin Caballero <aldrincaballero94@gmail.com>
2023-03-04 16:39:00 +0800

    some changes

--- build/index.js
+++ build/index.js
@@ -4,15 +4,31 @@
     cvs.height = game_height;
     cvs.width = game_width;
     const ctx = cvs.getContext("2d");
+    ctx.fillStyle = "000";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+    const mouse = { x: undefined, y: undefined };
     class Player {
-        constructor(x, y) {
+        constructor(x, y, height, width, src) {
             this.x = x;
             this.y = y;
+            this.height = height;
+            this.width = width;
+            this.src = src;
         }
         draw() {
             // drawing code here
         }
     }
-    const player = new Player(0, 0);
-    player.draw();
+    cvs.addEventListener("mousemove", (e) => {
+        mouse.x = e.x;
+        mouse.y = e.y;
+    });
+    cvs.addEventListener("touchmove", (e) => {
+        mouse.x = e.touches[0].clientX;
+        mouse.y = e.touches[0].clientY;
+    });
+    const player = new Player(0, 0, 100, 300, "");
+    const play = () => {
+        window.requestAnimationFrame(play);
+    };
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
-
 (() => {
   const cvs = document.getElementById("game-canvas") as HTMLCanvasElement;
-  let game_height: number = 600, game_width: number = 1200;
+  let game_height: number = 600,
+    game_width: number = 1200;
   cvs.height = game_height;
   cvs.width = game_width;
   const ctx = cvs.getContext("2d");
-  
+
+  ctx.fillStyle = "000";
+  ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+  const mouse = { x: undefined, y: undefined };
 
   class Player {
     x: number;
     y: number;
+    height: number;
+    width: number;
+    src: string;
 
-    constructor(x: number, y: number) {
+    constructor(
+      x: number,
+      y: number,
+      height: number,
+      width: number,
+      src: string
+    ) {
       this.x = x;
       this.y = y;
+      this.height = height;
+      this.width = width;
+      this.src = src;
     }
 
     draw() {
@@ -21,8 +37,18 @@
     }
   }
 
+  cvs.addEventListener("mousemove", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+  });
+  cvs.addEventListener("touchmove", (e) => {
+    mouse.x = e.touches[0].clientX;
+    mouse.y = e.touches[0].clientY;
+  });
 
-  const player = new Player(0, 0);
-  player.draw();
+  const player = new Player(0, 0, 100, 300, "");
 
+  const play = () => {
+    window.requestAnimationFrame(play);
+  };
 })();
